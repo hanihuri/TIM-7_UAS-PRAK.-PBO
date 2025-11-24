2258767e059399f07c523444fb71cacb18e884ca
hanihuri <huriyah24@mhs.usk.ac.id>
2025-11-24 21:43:30 +0700

    Update Akun.java

--- model/Akun.java
+++ model/Akun.java
@@ -1,1 +1,25 @@
+package model;
 
+public class Akun {
+    protected String id;
+    protected String username;
+    protected String password;
+
+    public Akun(String id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public boolean login(String u, String p) {
+        return username.equals(u) && password.equals(p);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
