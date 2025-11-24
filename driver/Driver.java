9b03923ced75aee535c729a0251e5f7a01ec6315
hanihuri <huriyah24@mhs.usk.ac.id>
2025-11-24 21:42:40 +0700

    Update Driver.java

--- driver/Driver.java
+++ driver/Driver.java
@@ -1,1 +1,13 @@
+package driver;
 
+import java.util.Scanner;
+
+public abstract class Driver {
+    protected Scanner scanner;
+
+    public Driver(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public abstract void menu();
+}
