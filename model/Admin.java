97a6a690546545fc7a6bf8c3cad73da9792d1b8f
hanihuri <huriyah24@mhs.usk.ac.id>
2025-11-24 23:09:12 +0700

    Update Admin.java

--- model/Admin.java
+++ model/Admin.java
@@ -1,8 +1,15 @@
 package model;
 
+/**
+ * Kelas Admin mewakili akun admin dan mewarisi atribut dasar dari Akun.
+ * Digunakan untuk operasi khusus admin dalam sistem.
+ */
+
+
 public class Admin extends Akun {
 
     public Admin(String id, String username, String password) {
         super(id, username, password);
     }
 }
+
