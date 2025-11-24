de9f26a1ce2e466cbf3db204a24cb22b9287de43
faelynss <fayla@mhs.usk.ac.id>
2025-11-24 21:21:46 +0700

    Update StatusTransaksi.java

--- model/StatusTransaksi.java
+++ model/StatusTransaksi.java
@@ -1,1 +1,24 @@
+package model;
+
+/**
+ * Enum yang merepresentasikan status dari sebuah transaksi.
+ * Setiap nilai menunjukkan tahapan proses transaksi dalam sistem.
+ */
+public enum StatusTransaksi {
+
+    /**
+     * Transaksi telah dibuat namun belum diproses lebih lanjut.
+     */
+    DIBUAT,
+
+    /**
+     * Transaksi menunggu konfirmasi dari admin sebelum dilanjutkan.
+     */
+    MENUNGGU_KONFIRMASI_ADMIN,
+
+    /**
+     * Transaksi telah selesai diproses dan dinyatakan berhasil.
+     */
+    SELESAI
+}
 
