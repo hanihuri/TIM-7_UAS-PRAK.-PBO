2dfb34d7f15084be0ee2f62e16110ec9d472a2e0
hanihuri <huriyah24@mhs.usk.ac.id>
2025-11-24 21:46:04 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,18 @@
+import view.LoginFrame;
+import javax.swing.*;
 
+public class Main {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new LoginFrame();
+            }
+        });
+    }
+}
